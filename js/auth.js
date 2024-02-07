ff0de11f43907dbc334dedd93b7b0eef4a59b8ff
almusfuad <almusfuad@gmail.com>
2024-02-07 23:55:40 +0600

    working on registration

--- js/auth.js
+++ js/auth.js
@@ -40,7 +40,32 @@ const login = (event) => {
 
 
 
-const register = () => {
+const register = (event) => {
+      event.preventDefault();
+      
+      // retrieve data
+      const username = document.getElementById('username').value;
+      const email = document.getElementById('email').value;
+      const firstName = document.getElementById('first_name').value;
+      const lastName = document.getElementById('last_name').value;
+      const password = document.getElementById('password').value;
+      const confirmPassword = document.getElementById('confirm_password').value;
+      const phoneNumber = document.getElementById('phone_no').value;
+      const profileImage = document.getElementById('image').files[0];
 
+      if (password !== confirmPassword) {
+            document.getElementById('password-error').innerHTML = 'Password do not match';
+      }
+
+      const formData = new FormData();
+      formData.append('username', username);
+      formData.append('email', email);
+      formData.append('first_name', firstName);
+      formData.append('last_name', lastName);
+      formData.append('password', password);
+      formData.append('phone_no', phoneNumber);
+      formData.append('image', profileImage);
+
+      fetch(``)
 }
 
