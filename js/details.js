fe947b4852b00d3993f09122be01670006d04165
almusfuad <almusfuad@gmail.com>
2024-02-09 23:29:57 +0600

    show details dynamically

--- js/details.js
+++ js/details.js
@@ -6,8 +6,32 @@ const getParams = () => {
             .then((res) => res.json())
             .then((data) => {
                   console.log(data);
+                  const dormitoryDetails = document.getElementById('details-container');
+                  const div = document.createElement('div');
+                  dormitoryDetails.innerHTML = `
+                  <div class="col-5 details-img">
+                        <img src="${data[0]?.image}" alt="Dormitory Image">
+                        <button >Book Now</button>
+                  </div>
+                  <div class="col-7 details-text">
+                        <h2><strong>${data[0]?.name}</strong></h2>
+                        <div class="facilities-type">
+                              <h3><strong>Facilities: </strong>${data[0]?.facilities}</h3>
+                              <h3><strong>Type: </strong>${data[0]?.dormitory_type}</h3>
+                        </div>
+                        <h3><Strong>Address: </Strong>${data[0]?.address}</h3>
+                        <div class="costs">
+                              <h3><strong>Per Night: </strong>$ ${data[0]?.per_night}</h3>
+                              <h3><strong>Per Month: </strong>$ ${data[0]?.per_month}</h3>
+                        </div>
+                        <h3><strong>Available Seats: </strong>${data[0]?.available_seats}</h3>
+                  </div>
+                  `;
             })
-}
+            .catch((err) => {
+                  console.error(err);
+            });
+};
 
 
 const details = (slug) => {
